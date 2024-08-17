275c2d3c4c01e7c13986af44269f10bdd28c024e
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-17 17:40:57 +0800

    Implement NameHandler

--- app/Services/Orders/NameHandler.php
+++ app/Services/Orders/NameHandler.php
@@ -1,12 +1,27 @@
 <?php
 
 namespace App\Services\Orders;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class NameHandler implements OrderFieldHandlerInterface
 {
     public function validate($value)
     {
-        
+        if (preg_match('/[^A-Za-z\s]/', $value)) {
+            throw new HttpResponseException(
+                response()->json(['message' => 'Name contains non-English characters'], JsonResponse::HTTP_BAD_REQUEST)
+            );
+        }
+
+        $words = explode(' ', $value);
+        foreach ($words as $word) {
+            if ($word[0] !== strtoupper($word[0])) {
+                throw new HttpResponseException(
+                    response()->json(['message' => 'Name is not capitalized'], JsonResponse::HTTP_BAD_REQUEST)
+                );
+            }
+        } 
     }
 
     public function transform($value)
