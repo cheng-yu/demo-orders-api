026ef85b1a8a87caad634a6e82c62bfd2a89e3b3
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-17 17:50:26 +0800

    Implement PriceHandler

--- app/Services/Orders/PriceHandler.php
+++ app/Services/Orders/PriceHandler.php
@@ -2,12 +2,18 @@
 
 namespace App\Services\Orders;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class PriceHandler implements OrderFieldHandlerInterface
 {
     public function validate($value)
     {
-        
+        if ($value > 2000) {
+            throw new HttpResponseException(
+                response()->json(['message' => 'Price is over 2000'], JsonResponse::HTTP_BAD_REQUEST)
+            );
+        }
     }
 
     public function transform($value)
