1f1ebffe244cef07464ec553a199ccf0c4f60d73
蔡承佑 <tim.tsai@kdanmobile.com>
2024-08-17 18:10:43 +0800

    Implement CurrencyHandler

--- app/Services/Orders/CurrencyHandler.php
+++ app/Services/Orders/CurrencyHandler.php
@@ -2,16 +2,32 @@
 
 namespace App\Services\Orders;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 
 class CurrencyHandler implements OrderFieldHandlerInterface
 {
+    public function __construct(PriceHandler $priceHandler)
+    {
+        $this->priceHandler = $priceHandler;
+    }
+
     public function validate($value)
     {
-        
+        if (!in_array($value, ['TWD', 'USD'])) {
+            throw new HttpResponseException(
+                response()->json(['message' => 'Currency format is wrong'], JsonResponse::HTTP_BAD_REQUEST)
+            );
+        }
     }
 
     public function transform($value)
     {
-        return $value; 
+        if ($value === 'USD') {
+            $this->priceHandler->validate($this->priceHandler->transform($price = request('price') * 31));
+            return 'TWD';
+        }
+
+        return $value;
     }
 }
